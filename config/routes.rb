bb33b772d97d7bac8f18a10cd06e0228c874d7f5
Savanna Bergen <savanna.bergen@gmail.com>
2024-11-02 14:32:14 -0500

    direct message logic added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "users/show"
   resources :rooms do
     resources :messages
   end
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "users", to: "devise/sessions#new"
   end
-  get "user/id", to: "users#show", as: "user"
+  get "user/:id", to: "users#show", as: "user"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
