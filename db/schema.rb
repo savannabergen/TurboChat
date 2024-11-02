2e12166af7528f70ba17e97414b3c858c4879dd9
Savanna Bergen <savanna.bergen@gmail.com>
2024-11-02 08:51:29 -0500

    Room model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_02_001312) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_134931) do
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
