5b639469aff12b035904b2bfe612ca4e41cc0811
Savanna Bergen <savanna.bergen@gmail.com>
2024-11-02 17:08:04 -0500

    scroll function fixed

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,19 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  /** On start */
   connect() {
     console.log("Connected");
-    const messages = document.getElementById("messages");
-    messages.addEventListener("DOMNodeInserted", this.resetScroll);
-    this.resetScroll(messages);
+    this.messages = document.getElementById("messages");
+    this.observeMessages();
+    this.resetScroll();
   }
-  /** On stop */
+
   disconnect() {
     console.log("Disconnected");
   }
-  /** Custom function */
-  resetScroll() {
-    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+
+  observeMessages() {
+    const observer = new MutationObserver(() => {
+      this.resetScroll();
+    });
+    observer.observe(this.messages, {
+      childList: true,
+    });
+  }
+
+  resetScroll = () => {
+    this.messages.scrollTop = this.messages.scrollHeight - this.messages.clientHeight;
   }
 }
