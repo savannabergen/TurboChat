0328ef8bc17718cc24fef480f324f8fd7ee23805
Savanna Bergen <savanna.bergen@gmail.com>
2024-11-02 10:46:01 -0500

    styles added, message model added, associations added to user and room, index updated

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,6 +5,19 @@ class RoomsController < ApplicationController
     @rooms = Room.public_rooms
 
     @users = User.all_except(current_user)
+    render "index"
+  end
+
+  def show
+    @single_room = Room.find(params[:id])
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    @users = User.all_except(current_user)
+    render "index"
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_02_134931) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_151355) do
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.boolean "is_private", default: false
@@ -29,4 +39,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_02_134931) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
