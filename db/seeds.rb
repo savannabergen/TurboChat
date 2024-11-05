aaf50ac4054dea1703697f9ce5256ca6d3e540a2
Savanna Bergen <savanna.bergen@gmail.com>
2024-11-04 19:26:14 -0600

    data injected

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,46 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'csv'
+require 'open-uri'
+require 'faker'
+
+csv_file_path = Rails.root.join('db', 'MOCK_DATA.csv')
+
+
+CSV.foreach(csv_file_path, headers: true) do |row|
+  next if row['email'].blank? || row['password'].blank?
+
+
+  user = User.create!(
+    email: row['email'],
+    password: row['password'],
+    password_confirmation: row['password']
+  )
+
+
+  avatar_url = row['avatar']
+  avatar_io = URI.open(avatar_url)
+  user.avatar.attach(io: avatar_io, filename: avatar_url.split('/').last)
+end
+
+
+# Create Rooms
+10.times do
+  room = Room.find_or_create_by!(name: Faker::Hacker.adjective)
+end
+
+
+# Create Participants and Messages
+User.all.each do |user|
+  Room.all.sample(3).each do |room|
+    participant = Participant.find_or_create_by!(user: user, room: room)
+    50.times do
+      Message.create!(
+        body: Faker::Lorem.paragraph(sentence_count: 2),
+        user: user,
+        room: room
+      )
+    end
+  end
+end
