94722ff624c627bad7522ef66ae5a744e6ef2987
Savanna Bergen <savanna.bergen@gmail.com>
2024-11-16 17:45:19 -0600

    ready to start user status - bug in the sign up profile pic

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem "bootsnap", require: false
 gem "image_processing", "~> 1.2"
 gem "mini_magick"
 gem "faker"
+gem "graphviz"
+gem "rails-erd"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,36 +17,50 @@ csv_file_path = Rails.root.join('db', 'MOCK_DATA.csv')
 CSV.foreach(csv_file_path, headers: true) do |row|
   next if row['email'].blank? || row['password'].blank?
 
-
-  user = User.create!(
+    user = User.create!(
     email: row['email'],
     password: row['password'],
     password_confirmation: row['password']
-  )
+    )
 
+    avatar_url = row['avatar']
+    avatar_io = URI.open(avatar_url)
+    user.avatar.attach(io: avatar_io, filename: avatar_url.split('/').last)
+    end
 
-  avatar_url = row['avatar']
-  avatar_io = URI.open(avatar_url)
-  user.avatar.attach(io: avatar_io, filename: avatar_url.split('/').last)
-end
+    admin_user = User.create!(
+    email: 'admin@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
 
+    admin_avatar_url = Faker::Avatar.image
+    admin_avatar_io = URI.open(admin_avatar_url)
+    admin_user.avatar.attach(io: admin_avatar_io, filename: admin_avatar_url.split('/').last)
 
-# Create Rooms
-10.times do
-  room = Room.find_or_create_by!(name: Faker::Hacker.adjective)
-end
+    normal_user = User.create!(
+    email: 'user@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
 
+    normal_avatar_url = Faker::Avatar.image
+    normal_avatar_io = URI.open(normal_avatar_url)
+    normal_user.avatar.attach(io: normal_avatar_io, filename: normal_avatar_url.split('/').last)
 
-# Create Participants and Messages
-User.all.each do |user|
-  Room.all.sample(3).each do |room|
+    20.times do
+    room = Room.find_or_create_by!(name: Faker::Hacker.adjective)
+    end
+
+    User.all.each do |user|
+    Room.all.sample(3).each do |room|
     participant = Participant.find_or_create_by!(user: user, room: room)
     50.times do
-      Message.create!(
-        body: Faker::Lorem.paragraph(sentence_count: 2),
-        user: user,
-        room: room
-      )
+    Message.create!(
+    body: Faker::Lorem.paragraph(sentence_count: 2),
+    user: user,
+    room: room
+    )
     end
   end
 end
