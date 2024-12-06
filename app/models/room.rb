7420d5ea3b1eaa43b9d4e7d8f029e64f382d665d
Savanna Bergen <savanna.bergen@gmail.com>
2024-12-06 08:37:15 -0600

    ready to hand in

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
   validates_uniqueness_of :name
   scope :public_rooms, -> { where(is_private: false) }
   after_create_commit { broadcast_if_public }
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_many :participants, dependent: :destroy
 
   def broadcast_if_public
