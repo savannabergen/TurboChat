51a61c671861ea1b71e0f9a18e4c9d62e0be3f97
Savanna Bergen <savanna.bergen@gmail.com>
2025-05-31 15:30:00 -0500

    try again with messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,6 @@ class MessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:body)
   end
 end
