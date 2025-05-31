ab5bd62b31a7c39379db08e30960a5c904290a7f
Savanna Bergen <savanna.bergen@gmail.com>
2025-05-30 20:35:04 -0500

    slimmed down message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,16 +2,13 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  before_create :confirm_participant
   after_create_commit :notify_clients
-
-  def confirm_participant
-    return unless room.is_private
-    is_participant = Participant.where(user_id: user.id, room_id: room.id).first
-    throw :abort unless is_participant
-  end
 
   def notify_clients
     $redis.publish("room:#{room.id}", { message: self }.to_json)
+  end
+
+  def confirm_participant
+  true
   end
 end
