ea3a8576562c8c0f78b27cbf1a351f61c3a1e254
Savanna Bergen <savanna.bergen@gmail.com>
2025-05-31 18:25:02 -0500

    got rid of all the bs i did today

--- app/controllers/auth_controller.rb
+++
@@ -1,9 +0,0 @@
-class AuthController < ApplicationController
-  def check
-    if user_signed_in?
-      render json: { authenticated: true }
-    else
-      render json: { authenticated: false }, status: :unauthorized
-    end
-  end
-end
