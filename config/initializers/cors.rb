98351b173b6f59ed1dae3d64a4f4de5e573b7ac4
Savanna Bergen <savanna.bergen@gmail.com>
2025-06-01 22:53:41 -0500

    cors.rb credentials r true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' # Replace with your Next.js application's domain
+    origins 'http://localhost:3000'
     resource '*',
       headers: :any,
       methods: %i[get post put patch delete options head],
-      expose: [:Authorization]
+      expose: [:Authorization],
+      credentials: true
   end
 end
