447c9694f9f83b0d7605e71eff1dbccc5aa39257
Savanna Bergen <savanna.bergen@gmail.com>
2025-06-01 23:46:59 -0500

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
-  },
-  controllers: {
+  }, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  resources :rooms do
+    member do
+      get :participants
+    end
+    resources :messages, only: [:index, :create]
+  end
+
+  resources :users, only: [:index]
 end
