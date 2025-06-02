4faa0067ea12330ec424c278d4f38f004eab7ae2
Savanna Bergen <savanna.bergen@gmail.com>
2025-06-02 13:58:36 -0500

    think my routes are right now

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  }, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
 
-  constraints format: :json do
-    resources :rooms, path: '/api/rooms' do
+  devise_for :users,
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' },
+             controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
+  namespace :api do
+    resources :rooms do
       member do
         get :participants
       end
-      resources :messages, path: '/api/rooms/:room_id/messages', only: [:index, :create]
+      resources :messages, only: [:index, :create]
     end
-    resources :users, path: '/api/users', only: [:index]
+    resources :users, only: [:index]
   end
 end
