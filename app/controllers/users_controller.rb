9add06c1a573dfcfe42d8ab479c68f486090526f
Savanna Bergen <savanna.bergen@gmail.com>
2025-06-03 18:15:57 -0500

    routes for avatar added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :update_avatar, :avatar]
 
   def index
     users = User.all
@@ -9,6 +9,26 @@ class UsersController < ApplicationController
 
   def show
     render json: { status: { code: 200, message: 'User retrieved successfully.' }, data: UserSerializer.new(@user).serializable_hash[:data] }, status: :ok
+  end
+
+  def me
+    render json: { status: { code: 200, message: 'Current user retrieved successfully.' }, data: UserSerializer.new(current_user).serializable_hash[:data] }, status: :ok
+  end
+
+  def avatar
+    if @user.avatar.attached?
+      redirect_to rails_blob_path(@user.avatar, disposition: 'attachment')
+    else
+      render json: { error: 'Avatar not found' }, status: :not_found
+    end
+  end
+
+  def update_avatar
+    if current_user.avatar.attach(params[:avatar])
+      render json: { status: { code: 200, message: 'Avatar updated successfully.' }, data: UserSerializer.new(current_user).serializable_hash[:data] }, status: :ok
+    else
+      render json: { status: { code: 422, message: 'Failed to update avatar.' }, errors: current_user.errors }, status: :unprocessable_entity
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
-  root to: 'home#index'
-
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' }, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
-
+  resources :users, only: [:index, :show] do
+    collection do
+      get :me
+      patch :update_avatar
+    end
+    member do
+      get :avatar
+    end
+  end
   resources :rooms, only: [:index, :show, :create] do
-  member do
-    get :participants
+    member do
+      get :participants
+    end
+    resources :messages, only: [:index, :create]
   end
-  resources :messages, only: [:index, :create]
-  end
+  root to: 'home#index'
 end
